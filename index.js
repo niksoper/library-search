2a7fd67f6d70f5fa1c75bf03f01ae6267d515987
Nick Soper <nsoper@scottlogic.com>
2017-01-07 10:56:32 +0000

    Factor out search function

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 console.log('Loading...')
 
 var jsdom = require('jsdom')
+
 var term = process.argv[2]
 
-var url = `https://www.librarieswest.org.uk/client/en_GB/default/search/results?qu=${term}&te=ILS&lm=BOOK&rt=false%7C%7C%7CTITLE%7C%7C%7CTitle` 
-var scripts = ['http://code.jquery.com/jquery.js']
-
 try {
+  search(term)
+} catch(e) {
+  console.log('caught', e)
+}
+
+function search(term) {
+  var url = `https://www.librarieswest.org.uk/client/en_GB/default/search/results?qu=${term}&te=ILS&lm=BOOK&rt=false%7C%7C%7CTITLE%7C%7C%7CTitle` 
+  var scripts = ['http://code.jquery.com/jquery.js']
+
   jsdom.env(
     url,
     scripts, 
-    function(err, window) {
-      reportResults(window)
-    }
-  );
-} catch(e) {
-  console.log('caught', e)
+    (err, window) => reportResults(window)
+  )
 }
 
 function reportResults(window) {
