9851a55b8e5d9215a644959a508f152f6c53aa32
Nick Soper <nsoper@scottlogic.com>
2017-01-07 10:51:06 +0000

    Factor out reportResults

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ console.log('Loading...')
 var jsdom = require('jsdom')
 var term = process.argv[2]
 
-
 var url = `https://www.librarieswest.org.uk/client/en_GB/default/search/results?qu=${term}&te=ILS&lm=BOOK&rt=false%7C%7C%7CTITLE%7C%7C%7CTitle` 
 var scripts = ['http://code.jquery.com/jquery.js']
 
@@ -11,12 +10,16 @@ try {
   jsdom.env(
     url,
     scripts, 
-    function (err, window) {
-      var results = window.jQuery('#results_wrapper a').toArray().map(l => l.href)
-      console.log(`${results.length} results`)
-      console.log(results)
+    function(err, window) {
+      reportResults(window)
     }
   );
 } catch(e) {
   console.log('caught', e)
+}
+
+function reportResults(window) {
+  var results = window.jQuery('#results_wrapper a').toArray().map(l => l.href)
+  console.log(`${results.length} results`)
+  console.log(results)
 }
