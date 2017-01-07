cc92f8b54d61270eef3bec384211cb3037a32287
Nick Soper <nsoper@scottlogic.com>
2017-01-07 17:26:12 +0000

    Use const not var

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var term = getTerm()
+const term = getTerm()
 
 if (!term) {
   console.log('No search term!')
@@ -7,7 +7,7 @@ if (!term) {
 
 console.log('Loading...')
 
-var jsdom = require('jsdom')
+const jsdom = require('jsdom')
 
 try {
   search(term)
@@ -20,8 +20,8 @@ function getTerm() {
 }
 
 function search(term) {
-  var url = `https://www.librarieswest.org.uk/client/en_GB/default/search/results?qu=${term}&te=ILS&lm=BOOK&rt=false%7C%7C%7CTITLE%7C%7C%7CTitle` 
-  var scripts = ['http://code.jquery.com/jquery.js']
+  const url = `https://www.librarieswest.org.uk/client/en_GB/default/search/results?qu=${term}&te=ILS&lm=BOOK&rt=false%7C%7C%7CTITLE%7C%7C%7CTitle` 
+  const scripts = ['http://code.jquery.com/jquery.js']
 
   console.log(`Searching for ${term}...`)
 
@@ -33,7 +33,7 @@ function search(term) {
 }
 
 function reportResults(window) {
-  var results = window.jQuery('#results_wrapper a').toArray().map(l => l.href)
+  const results = window.jQuery('#results_wrapper a').toArray().map(l => l.href)
   console.log(`${results.length} results`)
   console.log(results)
 }
