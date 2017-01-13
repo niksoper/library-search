72df66fca654ef522599da96da7b829ad9bf5f87
Nick Soper <nsoper@scottlogic.com>
2017-01-13 20:55:54 +0000

    Flatten with lodash

--- src/book.js
+++ src/book.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const request = require('./request')
 
 const importUri = 'https://extraction.import.io/query/extractor/1628e2be-0087-46ca-9fe3-1c23eb003470?_apikey=440e7c599cc244e5ae6b5275935704b8d39f5a2eb7ace86fb79deac135b000d93b2fdbb4f3ce694fa1906ad8424993f2ff73848f1408b4f52aa957078c7b4baf9ac1abede9339f0cfb9f0e257296a93f'
@@ -10,6 +11,7 @@ function getAvailability(bookUrls) {
   console.log(`Getting availability...`)
 
   return Promise.all(bookUrls.map(singleAvailability))
+    .then(_.flatten)
 }
 
 function singleAvailability(bookUrl) {
